e5ba2201cb484c31fcfae9496f4a6ed5fdf34f17
Kai Thompson <kaihrthompson@gmail.com>
2026-03-06 21:49:27 -0500

    scaffold API

---
+++ API/src/app.ts
@@ -0,0 +1,8 @@
+import express, { Application, Request, Response } from 'express';
+
+
+
+const app: Application = express();
+
+
+app.listen(4000, () => { console.log('Server running on port 4000') });
